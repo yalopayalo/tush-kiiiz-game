1cf9968fc97fa53d92607030793f85574cb504ee
yalopayalo <karabas311@gmail.com>
2021-03-30 23:17:37 +0300

    1

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-const imgEl = document.querySelector('img');
-imgEl.addEventListener('mouseenter', function(event) {
-imgEl.src = "meaning-05.jpg"
-});
+// const imgEl = document.querySelector('img');
+// imgEl.addEventListener('mouseenter', function(event) {
+// imgEl.src = "meaning-05.jpg"
+// });
+
+// const imgEl = document.querySelector('img');
+// imgEl.addEventListener('mouseleave', function(event) {
+// imgEl.src = "new_tush_kiiiz_colour_web-08.jpg"
+// });
